433f60a54f64e76a643823dfc4734713dd71a93b
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2018-01-11 02:21:04 -0500

    added photos, transferred script to script.js, created projectsstyle.css, updated all
    all pages active now, toDO: add projects to projects.html, fix experience.html, incorporate node.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,105 @@
-// JavaScript source code
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
 
-/*change link colors on hover*/
-/*active page*/
+$(document).ready(function() {
+    $(".dropdown").click(function() {
+        $(".dropdown-menu").slideDown(300);
+    });
+});
 
-//add animations
+$(document).click(function(e) {
+    e.stopPropagation();
+    var container = $(".dropdown");
+
+    if (container.has(e.target).length === 0) {
+        $(".dropdown-menu").slideUp(300);
+    }
+});
+
+! function() {
+    var t;
+    if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void(window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
+        t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
+        t.factory = function(e) {
+            return function() {
+                var n;
+                return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
+            };
+        }, t.methods.forEach(function(e) {
+            t[e] = t.factory(e);
+        }), t.load = function(t) {
+            var e, n, o, i;
+            e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"),
+                o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js",
+                n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
+        });
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('y5fd5ndskw39');
+
+$(document).ready(function() {
+    $(".emailname").hide();
+    $(".phonename").hide();
+});
+
+$(document).ready(function() {
+    $(".email").click(function() {
+        $(".emailname").fadeToggle(200);
+    });
+});
+$(document).ready(function() {
+    $(".phone").click(function() {
+        $(".phonename").fadeToggle(200);
+    });
+});
+
+
+function copyValue() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    document.execCommand('copy');
+}
+
+/*function ScrollToBottom() {
+            window.scrollTo(0, document.body.scrollHeight);
+        }
+
+        $(document).ready(function() {
+            $(".introtext").hide();
+        });
+        $(document).ready(function() {
+            $("footer").hide();
+        });
+
+        $(document).ready(function() {
+            $(".dropdown").hide();
+        });
+
+        $(document).ready(function() {
+            $(".link3").click(function() {
+                $(".dropdown").slideDown(300);
+            });
+        });
+
+        $(document).ready(function() {
+            $(".dropdown").mouseleave(function() {
+                $(".dropdown").slideUp(300);
+            });
+        });
+
+        $(document).ready(function() {
+            $("#arrow").click(function() {
+                $(".welcome").fadeIn(500);
+                $(".introtext").fadeIn(2000, "linear");
+            });
+        });
+
+        $(document).ready(function() {
+            $(".bg").mouseenter(function() {
+                $(".welcome").fadeIn(500);
+                $(".introtext").fadeIn(2000, "linear");
+                $("footer").fadeIn(2000);
+            });
+        });
+        */
