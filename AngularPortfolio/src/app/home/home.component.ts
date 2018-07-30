de1d2bf0ae0ab204e8798e07ab6aed35a566e515
djoksimo <djoksimo@uwaterloo.ca>
2018-07-30 19:18:50 -0400

    updated references and fixed minor details

--- AngularPortfolio/src/app/home/home.component.ts
+++ AngularPortfolio/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
-declare var Parallax: any;
-import * as Parallax from 'parallax-js';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +10,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ngAfterContentInit(){
-    const scene = document.getElementById('scene');
-    const parallaxInstance = new Parallax(scene, {
-      relativeInput: true,
-      hoverOnly: true
-    });
   }
 
 }
